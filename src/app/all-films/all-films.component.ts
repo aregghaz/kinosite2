94a41f94abd961323ca0681322f924a13c5819ff
areg <areg@cpainferno.com>
2018-11-05 16:12:58 +0400

    pagination

--- src/app/all-films/all-films.component.ts
+++ src/app/all-films/all-films.component.ts
@@ -1,37 +1,45 @@
-import {Component, Injectable, Input, OnInit} from '@angular/core';
+import {Component, Injectable, OnInit, AfterViewInit} from '@angular/core';
 import {AllFilmsService} from '../service/allFilms.service';
-import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import {PageChangedEvent} from 'ngx-bootstrap/pagination';
 
 
 @Injectable()
 @Component({
-  selector: 'app-all-films',
-  templateUrl: './all-films.component.html',
-  styleUrls: ['./all-films.component.scss']
+    selector: 'app-all-films',
+    templateUrl: './all-films.component.html',
+    styleUrls: ['./all-films.component.scss']
 })
 export class AllFilmsComponent implements OnInit {
 
- films:any = [];
- films2: any = [];
-  searhStr = '';
+    films: any = [];
+    films2: any = [];
+    searhStr = '';
 
-  maxSize = 5;
-  returnedArray;
-  constructor(private  allFilms: AllFilmsService ) { }
+    maxSize = 5;
+    returnedArray;
 
-  ngOnInit() {
-    this.allFilms.getAllMoves().subscribe(moves => {
-      this.films = moves;
+    constructor(private  allFilms: AllFilmsService) {
+    }
 
-      this.returnedArray = this.films.slice(0, 10);
-    });
-  }
+    ngOnInit() {
+        this.allFilms.getAllMoves().subscribe(moves => {
+            this.films = moves;
+            this.returnedArray = this.films.slice(0, 10);
+        });
+    }
 
 
+    pageChanged(event: PageChangedEvent): void {
+        const startItem = (event.page - 1) * event.itemsPerPage;
+        const endItem = event.page * event.itemsPerPage;
+        this.returnedArray = this.films.slice(startItem, endItem);
+    }
 
-  pageChanged(event: PageChangedEvent): void {
-    const startItem = (event.page - 1) * event.itemsPerPage;
-    const endItem = event.page * event.itemsPerPage;
-    this.returnedArray = this.films.slice(startItem, endItem);
-  }
+    // ngAfterViewInit() {
+    //     this.allFilms.getAllMoves().subscribe(moves => {
+    //         this.films = moves;
+    //         console.log(this.films);
+    //         this.returnedArray = this.films.slice(0, 10);
+    //     });
+    // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,74 +1,79 @@
 import {BrowserModule} from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { MovesComponent } from './moves/moves.component';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {MovesComponent} from './moves/moves.component';
 import {HttpModule} from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { SerialsComponent } from './serials/serials.component';
+import {FormsModule} from '@angular/forms';
+import {SerialsComponent} from './serials/serials.component';
 import {MovesService} from './service/moves.service';
 import {SerialsService} from './service/serials.service';
-import { HttpClientModule} from '@angular/common/http';
-import { HomePageComponent } from './home-page/home-page.component';
-import { RouterModule} from '@angular/router';
-import { SingleSerialComponent } from './single-serial/single-serial.component';
-import { SingleMovesComponent } from './single-moves/single-moves.component';
-import { SingleSerialsService } from './service/singleSerial.service';
-import { SingleMoveService } from './service/singleMove.service';
-import { MessageComponent } from './message/message.component';
-import { CopyrightComponent } from './copyright/copyright.component';
-import { AdvertisementComponent } from './advertisement/advertisement.component';
-import { FilmsPipe } from './pipe/films.pipe';
+import {HttpClientModule} from '@angular/common/http';
+import {HomePageComponent} from './home-page/home-page.component';
+import {RouterModule} from '@angular/router';
+import {SingleSerialComponent} from './single-serial/single-serial.component';
+import {SingleMovesComponent} from './single-moves/single-moves.component';
+import {SingleSerialsService} from './service/singleSerial.service';
+import {SingleMoveService} from './service/singleMove.service';
+import {MessageComponent} from './message/message.component';
+import {CopyrightComponent} from './copyright/copyright.component';
+import {AdvertisementComponent} from './advertisement/advertisement.component';
+import {FilmsPipe} from './pipe/films.pipe';
 import {SerialsPipe} from './pipe/serials.pipe';
-import { AllFilmsComponent } from './all-films/all-films.component';
+import {AllFilmsComponent} from './all-films/all-films.component';
 import {AllFilmsService} from './service/allFilms.service';
-import { AllSerialsComponent } from './all-serials/all-serials.component';
+import {AllSerialsComponent} from './all-serials/all-serials.component';
 import {AllSerialsService} from './service/allSerials.service';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
+import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+
 const routes = [
-  {path: '', pathMatch: 'full', component: HomePageComponent},
-  {path: 'serial/:',  component: SingleSerialComponent},
-  {path: 'film/:',  component: SingleMovesComponent},
-  {path: 'message',  component: MessageComponent},
-  {path: 'copyright',  component: CopyrightComponent},
-  {path: 'advertisement',  component: AdvertisementComponent},
-  {path: 'allFilms',  component: AllFilmsComponent},
-  {path: 'allSerials',  component: AllSerialsComponent},
+    {path: '', pathMatch: 'full', component: HomePageComponent},
+    {path: 'serial/:', component: SingleSerialComponent},
+    {path: 'film/:', component: SingleMovesComponent},
+    {path: 'message', component: MessageComponent},
+    {path: 'copyright', component: CopyrightComponent},
+    {path: 'advertisement', component: AdvertisementComponent},
+    {path: 'allFilms', component: AllFilmsComponent},
+    {path: 'allSerials', component: AllSerialsComponent},
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MovesComponent,
-    SerialsComponent,
-    HomePageComponent,
-    SingleSerialComponent,
-    SingleMovesComponent,
-    MessageComponent,
-    CopyrightComponent,
-    AdvertisementComponent,
-    FilmsPipe,
-    SerialsPipe,
-    AllFilmsComponent,
-    AllSerialsComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    PaginationModule.forRoot()
-  ],
-  providers: [
-    MovesService,
-    SerialsService,
-    SingleSerialsService,
-    SingleMoveService,
-    AllFilmsService,
-    AllSerialsService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    declarations: [
+        AppComponent,
+        MovesComponent,
+        SerialsComponent,
+        HomePageComponent,
+        SingleSerialComponent,
+        SingleMovesComponent,
+        MessageComponent,
+        CopyrightComponent,
+        AdvertisementComponent,
+        FilmsPipe,
+        SerialsPipe,
+        AllFilmsComponent,
+        AllSerialsComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot(routes),
+        PaginationModule.forRoot(),
+    ],
+    providers: [
+        MovesService,
+        SerialsService,
+        SingleSerialsService,
+        SingleMoveService,
+        AllFilmsService,
+        AllSerialsService
+    ],
+    bootstrap: [
+        AppComponent,
+
+    ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
