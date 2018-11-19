300bf3d4d0f5809202a272c6728c49d32d324a5b
aregghaz@gmail.com <gagulik014>
2018-11-19 13:08:25 +0400

    asd

---
+++ src/assets/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ashotkazaryan on 18.11.18.
+ */
+
+
+$(document).ready(function () {
+    var iframe = document.getElementById("myIframe");
+    var elmnt = iframe.contentWindow.document.getElementsByTagName("body");
+    console.log(iframe)
+    console.log(elmnt)
+//elmnt.style.display = "none";
+})
